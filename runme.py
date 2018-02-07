5600d1473626461c97940ca2c9cc48c35e9b799d
Kaorixx <midumulf@gmail.com>
2018-02-07 13:01:35 -0500

    Add files via upload

---
+++ runme.py
@@ -0,0 +1,7 @@
+print ("hello, user")
+print ("How did you find this file?")
+print ("Moreover, how did you get into this server?")
+print ("g e t o u t n o w")
+input()
+
+#Get out of my code you fuckin monkey
